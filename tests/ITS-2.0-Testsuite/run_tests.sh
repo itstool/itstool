a6becc8bbc4ce20217ba6a8bd11016297db2e607
Shaun McCance <shaunm@gnome.org>
2013-02-02 14:15:56 -0500

    Adding the ITS 2.0 test suite, found here:
    https://github.com/finnle/ITS-2.0-Testsuite/commits/master

---
+++ tests/ITS-2.0-Testsuite/run_tests.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+testdata="
+elementswithintext/withintext/withinText
+externalresource/externalresource/externalResourceRef
+idvalue/idvalue/idValue
+localefilter/locale/localeFilterList
+localizationnote/locnote/locNote
+preservespace/preservespace/preserveSpace
+translate/translate/translate
+"
+
+exclude="
+locnote1xml
+locnote2xml
+locnote3xml
+locnote4xml
+locnote5xml
+locnote6xml
+locnote7xml
+locnote8xml
+locnote9xml
+
+externalresource4xml
+externalresource5xml
+idvalue4xml
+idvalue5xml
+locale7xml
+locnote10xml
+locnote11xml
+preservespace5xml
+preservespace6xml
+translate9xml
+translate10xml
+withintext5xml
+withintext6xml
+"
+
+for datum in $testdata; do
+    for format in xml; do
+        testdir=`echo $datum | cut -d/ -f1`
+        testbase=`echo $datum | cut -d/ -f2`
+        testcat=`echo $datum | cut -d/ -f3`
+        testpre="inputdata/$testdir/$format/$testbase"
+        for testfile in `ls ${testpre}*${format}.${format}`; do
+            testnum=`basename ${testfile:${#testpre}} ${format}.${format}`
+            skip=0
+            for ex in $exclude; do
+                if [ "$testbase$testnum$format" = "$ex" ]; then skip=1; break; fi;
+            done
+            if [ "$skip" = "0" ]; then
+                expected="expected/$testdir/$format/$testbase$testnum${format}output.txt"
+                realout="realout/$testdir/$format/$testbase$testnum${format}output.txt"
+                mkdir -p "realout/$testdir/$format"
+                python ../../itstool.in -n -t $testcat -o $realout $testfile
+                if ! cmp "$expected" "$realout"; then
+                    echo "$expected";
+                    exit 1;
+                fi
+            fi
+        done
+    done
+done
